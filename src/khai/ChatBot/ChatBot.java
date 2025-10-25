bd39ea79ffadb46882d2941a844433712bb99fbf
Tymofii <t.o.koliesnikov@student.khai.edu>
2025-10-25 20:27:42 +0300

    ChatBot 1st stage

---
+++ src/khai/ChatBot/ChatBot.java
@@ -0,0 +1,11 @@
+package khai.ChatBot;
+
+public class ChatBot {
+    public static void main(String[] args) {
+        String botName = "Sheridan";
+        int birthYear = 2025;
+
+        System.out.println("Hello! My name is " + botName + ".");
+        System.out.println("I was created in " + birthYear + ".");
+    }
+}
