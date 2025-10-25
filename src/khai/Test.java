c591b5cc93cf41baad9c07b240162641a869a29b
Tymofii <t.o.koliesnikov@student.khai.edu>
2025-10-25 18:40:02 +0300

    Test.java added to feature branch
    # Conflicts:
    #	src/khai/Test.java
    
    added one more print to Test.java on the feature branch

--- src/khai/Test.java
+++ src/khai/Test.java
@@ -2,6 +2,8 @@ package khai;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Java");
+        System.out.println("education");
+        //comment
+        System.out.print("DICT");
     }
 }
