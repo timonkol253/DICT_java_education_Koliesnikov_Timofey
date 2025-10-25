05a1518667da0cd017d052b10ee5573b4f785d94
Tymofii <t.o.koliesnikov@student.khai.edu>
2025-10-25 20:29:15 +0300

    ChatBot 2nd stage

--- src/khai/ChatBot/ChatBot.java
+++ src/khai/ChatBot/ChatBot.java
@@ -1,5 +1,7 @@
 package khai.ChatBot;
 
+import java.util.Scanner;
+
 public class ChatBot {
     public static void main(String[] args) {
         String botName = "Sheridan";
@@ -7,5 +9,12 @@ public class ChatBot {
 
         System.out.println("Hello! My name is " + botName + ".");
         System.out.println("I was created in " + birthYear + ".");
+        System.out.println("Please, remind me your name.");
+
+        Scanner scanner = new Scanner(System.in);
+        String userName = scanner.nextLine();
+
+        System.out.println("What a great name you have, " + userName + "!");
     }
 }
+
