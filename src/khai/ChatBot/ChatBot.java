142fdb93491411261bef85ffcd4079191391ffb5
Tymofii <t.o.koliesnikov@student.khai.edu>
2025-10-26 13:21:07 +0200

    ChatBot 5th stage

--- src/khai/ChatBot/ChatBot.java
+++ src/khai/ChatBot/ChatBot.java
@@ -24,16 +24,32 @@ public class ChatBot {
         int remainder7 = scanner.nextInt();
 
         int age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
-
         System.out.println("Your age is " + age + "; that's a good time to start programming!");
 
         System.out.println("Now I will prove to you that I can count to any number you want!");
-
         int number = scanner.nextInt();
 
         for (int i = 0; i <= number; i++) {
             System.out.println(i + "!");
         }
+
+        System.out.println("Let's test your programming knowledge.");
+        System.out.println("Which keyword is used to define a class in Java?");
+        System.out.println("1. function");
+        System.out.println("2. define");
+        System.out.println("3. class");
+        System.out.println("4. object");
+
+        int answer;
+        do {
+            answer = scanner.nextInt();
+            if (answer != 3) {
+                System.out.println("That's not correct. Please, try again.");
+            }
+        } while (answer != 3);
+
+        System.out.println("Congratulations, you are absolutely right!");
+        System.out.println("Goodbye, have a nice day!");
     }
 }
 
