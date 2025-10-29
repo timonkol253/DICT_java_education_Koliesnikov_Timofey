44ae6692ad502055fcb5d6bf1212573e8191e242
Tymofii <t.o.koliesnikov@student.khai.edu>
2025-10-29 22:55:46 +0200

    Hangman 3rd stage

--- src/khai/Hangman/Hangman.java
+++ src/khai/Hangman/Hangman.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 
 public class Hangman {
     public static void main(String[] args) {
-        // Список слів
         String[] words = {"python", "java", "javascript", "kotlin"};
 
         // Випадковий вибір слова
