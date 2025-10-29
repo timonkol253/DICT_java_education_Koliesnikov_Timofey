9b46ccf2ac0d0562f17ec7815e1b477f9364fa8f
Tymofii <t.o.koliesnikov@student.khai.edu>
2025-10-29 22:49:23 +0200

    Hangman 2nd stage

--- src/khai/Hangman/Hangman.java
+++ src/khai/Hangman/Hangman.java
@@ -1,8 +1,32 @@
 package khai.Hangman;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Hangman {
     public static void main(String[] args) {
+        // Список слів
+        String[] words = {"python", "java", "javascript", "kotlin"};
+
+        // Випадковий вибір слова
+        Random random = new Random();
+        String secretWord = words[random.nextInt(words.length)];
+
+        // Привітання та запит
         System.out.println("HANGMAN");
-        System.out.println("The game will be available soon.");
+        System.out.print("Guess the word: > ");
+
+        // Зчитування введення
+        Scanner scanner = new Scanner(System.in);
+        String guess = scanner.nextLine();
+
+        // Перевірка
+        if (guess.equals(secretWord)) {
+            System.out.println("You survived!");
+        } else {
+            System.out.println("You lost!");
+        }
+
+        scanner.close();
     }
 }
