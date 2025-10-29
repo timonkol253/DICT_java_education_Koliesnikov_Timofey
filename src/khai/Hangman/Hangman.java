a9d4dc0949f225cd39c0151e493df522d361eb5c
Tymofii <t.o.koliesnikov@student.khai.edu>
2025-10-29 22:47:21 +0200

    Hangman 1st stage

---
+++ src/khai/Hangman/Hangman.java
@@ -0,0 +1,8 @@
+package khai.Hangman;
+
+public class Hangman {
+    public static void main(String[] args) {
+        System.out.println("HANGMAN");
+        System.out.println("The game will be available soon.");
+    }
+}
