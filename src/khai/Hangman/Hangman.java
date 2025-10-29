d63c89b49ba8bc96cb089f7ae4f138155d922aa7
Tymofii <t.o.koliesnikov@student.khai.edu>
2025-10-29 23:18:15 +0200

    Hangman 5th stage

--- src/khai/Hangman/Hangman.java
+++ src/khai/Hangman/Hangman.java
@@ -6,31 +6,39 @@ import java.util.Scanner;
 public class Hangman {
     public static void main(String[] args) {
         String[] words = {"python", "java", "javascript", "kotlin"};
-
         Random random = new Random();
         String secretWord = words[random.nextInt(words.length)];
 
-        // Створюємо підказку: перші 2 літери + решта '-'
-        StringBuilder hint = new StringBuilder();
-        for (int i = 0; i < secretWord.length(); i++) {
-            if (i < 2) {
-                hint.append(secretWord.charAt(i));
-            } else {
-                hint.append('-');
-            }
+        char[] guessedWord = new char[secretWord.length()];
+        for (int i = 0; i < guessedWord.length; i++) {
+            guessedWord[i] = '-';
         }
-
-        System.out.println("HANGMAN");
-        System.out.print("Guess the word " + hint + ": > ");
 
         Scanner scanner = new Scanner(System.in);
-        String guess = scanner.nextLine().trim(); // ← trim() видаляє пробіли на початку/в кінці
+        System.out.println("HANGMAN");
 
-        if (guess.equals(secretWord)) {
-            System.out.println("You survived!");
-        } else {
-            System.out.println("You lost!");
+        for (int attempt = 0; attempt < 8; attempt++) {
+            System.out.println(new String(guessedWord));
+            System.out.print("Input a letter: > ");
+
+            char letter = scanner.nextLine().charAt(0);
+
+            boolean found = false;
+            for (int i = 0; i < secretWord.length(); i++) {
+                if (secretWord.charAt(i) == letter) {
+                    guessedWord[i] = letter;
+                    found = true;
+                }
+            }
+
+            if (!found) {
+                System.out.println("That letter doesn't appear in the word");
+            }
+            System.out.println();
         }
+
+        System.out.println("Thanks for playing!");
+        System.out.println("We'll see how well you did in the next stage");
 
         scanner.close();
     }
