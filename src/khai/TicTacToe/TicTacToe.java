310ff557bfad57d36632a10b1352ecb9aa2adb90
Tymofii <t.o.koliesnikov@student.khai.edu>
2025-10-30 19:53:15 +0200

    TicTacToe 2nd stage

--- src/khai/TicTacToe/TicTacToe.java
+++ src/khai/TicTacToe/TicTacToe.java
@@ -1,9 +1,17 @@
 package khai.TicTacToe;
 
+import java.util.Scanner;
+
 public class TicTacToe {
     public static void main(String[] args) {
-        System.out.println("X O X");
-        System.out.println("O X O");
-        System.out.println("X X O");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter cells: ");
+        String cells = scanner.nextLine();
+
+        System.out.println("---------");
+        System.out.println("| " + cells.charAt(0) + " " + cells.charAt(1) + " " + cells.charAt(2) + " |");
+        System.out.println("| " + cells.charAt(3) + " " + cells.charAt(4) + " " + cells.charAt(5) + " |");
+        System.out.println("| " + cells.charAt(6) + " " + cells.charAt(7) + " " + cells.charAt(8) + " |");
+        System.out.println("---------");
     }
 }
